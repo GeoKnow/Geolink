36aaa07143faa3acd7a325e9082411a83963616e
cstadler <cstadler@informatik.uni-leipzig.de>
2015-06-02 13:25:19 +0200

    Created scripts for creating test data

---
+++ test-data/create-dataset-dbpedia.sh
@@ -0,0 +1,3 @@
+rapper -o ntriples http://dbpedia.org/resource/Leipzig/Halle_Airport
+rapper -o ntriples http://dbpedia.org/resource/London_Stansted_Airport
+rapper -o ntriples http://dbpedia.org/resource/Incheon_International_Airport
---
+++ test-data/create-dataset-lgd.sh
@@ -0,0 +1,3 @@
+rapper -o ntriples http://linkedgeodata.org/triplify/node2702484159
+rapper -o ntriples http://linkedgeodata.org/triplify/way141145797 
+rapper -o ntriples http://linkedgeodata.org/triplify/node124332600
